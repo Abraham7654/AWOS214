554cefd8ae344af424c1a6946b2346b90b6ed02e
Abraham7654 <124049257@upq.edu.mx>
2026-01-27 19:39:16 -0600

    Actividad No.2: Contrato de aprendizaje

---
+++ miAPI/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import asyncio
+
+app = FastAPI()
+
+
+@app.get("/")
+async def bienvenida():
+    return {"mensaje": "¡Bienvenido a mi API!"}
+
+
+@app.get("/HolaMundo")
+async def hola():
+    await asyncio.sleep(4)
+    return {"mensaje": "¡Hola Mundo FastAPI"
+    "estatus":"200"}
